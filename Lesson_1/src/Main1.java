874731e93fb676c0996927489fa9ed5e6dcc23d6
Marlen Mels <marlenmels9@gmail.com>
2025-04-03 06:33:59 +0500

    First and second lesson

---
+++ Lesson_1/src/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("**************************");
+        System.out.println("****\tВизитка \t****");
+        System.out.println("**** Name: Marlen ****");
+        System.out.println("Skype: my_skype ****");
+        System.out.println("**************************");
+    }
+}
---
+++ Lesson_1/src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.print("**************************\n" +
+                "****\tВизитка \t****\n" +
+                "**** Name: \"Marlen\" ****\n" +
+                "Skype: my_skype ****\n" +
+                "**************************");
+    }
+}
