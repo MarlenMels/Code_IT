2c08a90d443af4172aad5b8bd63a42c2d50c97df
MarlenMels <marlenmels9@gmail.com>
2026-07-21 00:13:14 +0500

    Changed code and made much more better

--- Lesson9/src/HW9/Task2/src/Main2.java
+++ Lesson9/src/HW9/Task2/src/Main2.java
@@ -3,28 +3,21 @@ package HW9.Task2.src;
 import java.util.Scanner;
 
 public class Main2 {
-    public static int inputNum;
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
-        inputNum = input();
-        checkNum();
+        print();
     }
 
-    private static int input() {
-        System.out.println("Enter the input number: ");
+    public static int getInput(){
+        System.out.println("Enter a number: ");
         return new Scanner(System.in).nextInt();
     }
 
-    public static void checkNum(){
-        if(inputNum % 2 == 0){
-            System.out.println("Your number is even");
-            System.out.println(true);
-        } else {
-            System.out.println("Your number is odd");
-            System.out.println(false);
-        }
+    public static void print(){
+        System.out.println(getInput() % 2 == 0);
     }
+
 }
