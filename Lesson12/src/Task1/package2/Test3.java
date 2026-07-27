3e0b35a91b3c470716ad3a929e00c8eec08c8443
MarlenMels <marlenmels9@gmail.com>
2026-07-27 16:58:04 +0500

    Homework done!

--- Lesson12/src/Task1/package2/Test3.java
+++ Lesson12/src/Task1/package2/Test3.java
@@ -5,7 +5,7 @@ import Task1.package1.Test1;
 public class Test3 {
     public static void main(String[] args) {
         Test1 test1 = new Test1();
-        
+
         //here I cannot use the parameters of test1
     }
 }
